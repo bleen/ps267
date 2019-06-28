e1674cd959b4d3eefc4ff272cfa325c32e05c2a3
bleen <git.119x@bleen.net>
2019-06-28 10:22:38 -0400

    Updating focal point

--- web/modules/contrib/focal_point/src/Controller/FocalPointPreviewController.php
+++ web/modules/contrib/focal_point/src/Controller/FocalPointPreviewController.php
@@ -11,6 +11,11 @@ use Symfony\Component\Validator\Exception\InvalidArgumentException;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Url;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\OpenModalDialogCommand;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Image\ImageFactory;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Class FocalPointPreviewController.
@@ -20,15 +25,59 @@ use Drupal\Core\Url;
 class FocalPointPreviewController extends ControllerBase {
 
   /**
+   * The image factory service.
+   *
+   * @var \Drupal\Core\Image\ImageFactory
+   */
+  protected $imageFactory;
+
+  /**
+   * The request service.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $request;
+
+  /**
+   * The file storage service.
+   *
+   * @var \Drupal\file\FileStorage
+   */
+  protected $fileStorage;
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Drupal\Core\Image\ImageFactory $image_factory
+   *   The image_factory parameter.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request parameter.
+   */
+  public function __construct(ImageFactory $image_factory, RequestStack $request_stack) {
+    $this->imageFactory = $image_factory;
+    $this->request = $request_stack->getCurrentRequest();
+    $this->fileStorage = $this->entityManager()->getStorage('file');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('image.factory'),
+      $container->get('request_stack')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    *
    * @throws \Symfony\Component\Validator\Exception\InvalidArgumentException
    */
   public function content($fid, $focal_point_value) {
     $output = [];
-
-    $file = File::load($fid);
-    $image = \Drupal::service('image.factory')->get($file->getFileUri());
+    $file = $this->fileStorage->load($fid);
+    $image = $this->imageFactory->get($file->getFileUri());
     if (!$image->isValid()) {
       throw new InvalidArgumentException('The file with id = $fid is not an image.');
     }
@@ -47,7 +96,7 @@ class FocalPointPreviewController extends ControllerBase {
     $original_image = [
       '#theme' => 'image',
       '#uri' => $image->getSource(),
-      '#alt' => t('Focal Point Preview Image'),
+      '#alt' => $this->t('Focal Point Preview Image'),
       '#attributes' => [
         'id' => 'focal-point-preview-image',
       ],
@@ -76,7 +125,12 @@ class FocalPointPreviewController extends ControllerBase {
     else {
       // There are no styles that use a focal point effect to preview.
       $image_styles_url = Url::fromRoute('entity.image_style.collection')->toString();
-      drupal_set_message($this->t('You must have at least one <a href=":url">image style</a> defined that uses a focal point effect in order to preview.', [':url' => $image_styles_url]), 'warning');
+      drupal_set_message(
+        $this->t('You must have at least one <a href=":url">image style</a> defined that uses a focal point effect in order to preview.',
+          [':url' => $image_styles_url]
+        ),
+        'warning'
+      );
     }
 
     $output['focal_point_preview_page'] = [
@@ -88,7 +142,18 @@ class FocalPointPreviewController extends ControllerBase {
       '#derivative_image_note' => $derivative_image_note,
     ];
 
-    return $output;
+    $html = render($output);
+
+    $options = [
+      'dialogClass' => 'popup-dialog-class',
+      'width' => '80%',
+    ];
+    $response = new AjaxResponse();
+    $response->addCommand(
+      new OpenModalDialogCommand($this->t('Images preview'), $html, $options)
+    );
+
+    return $response;
   }
 
   /**
@@ -104,15 +169,15 @@ class FocalPointPreviewController extends ControllerBase {
    * @param int $fid
    *   The file id for the image being previewed from the URL.
    *
-   * @return AccessResult
+   * @return \Drupal\Core\Access\AccessResult
    *   An AccessResult object defining if permission is granted or not.
    */
   public function access(AccountInterface $account, $fid) {
     $access = AccessResult::forbidden();
 
     // @todo: I should be able to use "magic args" to load the file directly.
-    $file = File::load($fid);
-    $image = \Drupal::service('image.factory')->get($file->getFileUri());
+    $file = $this->fileStorage->load($fid);
+    $image = $this->imageFactory->get($file->getFileUri());
     if (!$image->isValid()) {
       throw new InvalidArgumentException('The file with id = $fid is not an image.');
     }
@@ -120,7 +185,7 @@ class FocalPointPreviewController extends ControllerBase {
     // Check if there was a valid token provided in with the HTTP request so
     // that preview is available on a "create entity" form.
     if ($this->validTokenProvided()) {
-      $access = AccessResult::allowed();;
+      $access = AccessResult::allowed();
     }
 
     // If access has not yet been granted and the file module is enabled, check
@@ -128,7 +193,7 @@ class FocalPointPreviewController extends ControllerBase {
     // has access to edit.
     if (function_exists('file_get_file_references') && !$access->isAllowed()) {
       $references = file_get_file_references($file, NULL, EntityStorageInterface::FIELD_LOAD_REVISION, '');
-      foreach ($references as $field_name => $data) {
+      foreach ($references as $data) {
         foreach (array_keys($data) as $entity_type_id) {
           if ($account->hasPermission($entity_type_id . ".edit")) {
             $access = AccessResult::allowed();
@@ -200,8 +265,13 @@ class FocalPointPreviewController extends ControllerBase {
    */
   protected function validTokenProvided() {
     try {
-      $token = \Drupal::request()->query->get('focal_point_token');
-      return FocalPointImageWidget::validatePreviewToken($token);
+      if (\Drupal::request()->query->has('focal_point_token')) {
+        $token = \Drupal::request()->query->get('focal_point_token');
+        return FocalPointImageWidget::validatePreviewToken($token);
+      }
+      else {
+        return FALSE;
+      }
     }
     catch (\InvalidArgumentException $e) {
       return FALSE;
